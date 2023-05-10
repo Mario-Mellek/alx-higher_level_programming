11807b914d4973cd9b48762ca7e72d6a5142a9aa
Mario-Mellek <mario.samy266@gmail.com>
2023-05-09 19:52:46 -0700

    Added a variable to fix the long print

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 
 for i in range(25, -1, -1):
-    print("{}".format(chr(i + ord('A')) if i % 2 == 0 else chr(i + ord('a'))), end='')
+    letter = chr(i + ord("A")) if i % 2 == 0 else chr(i + ord("a"))
+    print("{}".format(letter), end="")
