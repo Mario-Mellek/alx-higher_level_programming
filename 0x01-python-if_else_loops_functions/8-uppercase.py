a4660bd5ae5120ccc184120686f046a02f046942
Mario-Mellek <mario.samy266@gmail.com>
2023-05-09 18:39:35 -0700

    editing function ouput so that it doesn't print end at the last char

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for c in str:
+    strLen = len(str)
+    for i in range(strLen):
+        c = str[i]
         if ord(c) >= ord('a') and ord(c) <= ord('z'):
             c = chr(ord(c) - 32)
-        print("{}".format(c), end="")
-    print('\n')
+
+        if i == strLen - 1:
+            print("{}".format(c))
+        else:
+            print("{}".format(c), end="")
+    print("\n")
