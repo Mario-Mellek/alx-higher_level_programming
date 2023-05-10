e95068a189b7f915933068dc7204e86be3b925c8
Mario-Mellek <mario.samy266@gmail.com>
2023-05-09 18:44:09 -0700

    used conditional expression inside the print function to achieve the same result with only one print statement inside the loop

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,11 +3,6 @@ def uppercase(str):
     strLen = len(str)
     for i in range(strLen):
         c = str[i]
-        if ord(c) >= ord('a') and ord(c) <= ord('z'):
+        if ord(c) >= ord("a") and ord(c) <= ord("z"):
             c = chr(ord(c) - 32)
-
-        if i == strLen - 1:
-            print("{}".format(c))
-        else:
-            print("{}".format(c), end="")
-    print("\n")
+        print("{}".format(c), end="" if i != strLen - 1 else "\n")
