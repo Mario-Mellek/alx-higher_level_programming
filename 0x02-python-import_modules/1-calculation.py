92009c3a54dc69b8794d6e2451963b12f61b74a2
Mario-Mellek <mario.samy266@gmail.com>
2023-05-11 22:10:33 +0000

    Added main guard

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-import calculator_1 as cal
 
-a = 10
-b = 5
+if __name__ == "__main__":
+    import calculator_1 as cal
 
-print("{} + {} = {}".format(a, b, cal.add(a, b)))
-print("{} - {} = {}".format(a, b, cal.sub(a, b)))
-print("{} * {} = {}".format(a, b, cal.mul(a, b)))
-print("{} / {} = {}".format(a, b, cal.div(a, b)))
+    a = 10
+    b = 5
+
+    print("{} + {} = {}".format(a, b, cal.add(a, b)))
+    print("{} - {} = {}".format(a, b, cal.sub(a, b)))
+    print("{} * {} = {}".format(a, b, cal.mul(a, b)))
+    print("{} / {} = {}".format(a, b, cal.div(a, b)))
