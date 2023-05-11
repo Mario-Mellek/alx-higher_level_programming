615120d4ee0930ccc128e7d6ab265cc183ca1eeb
Mario-Mellek <mario.samy266@gmail.com>
2023-05-11 23:55:01 +0000

    Correct output
    my_secret_santa
    print_hidden
    print_school

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+    allNames = dir(hidden_4)
+    for n in allNames:
+        if not n.startswith("__"):
+            print(n)
+    
