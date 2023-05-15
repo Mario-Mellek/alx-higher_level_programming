a8202b01f85a9c61dad491b397793255e20af797
Mario-Mellek <mario.samy266@gmail.com>
2023-05-15 21:19:06 +0000

    Adjusted the first value
    strings are immutable in Python, so I assigned the value to a seperate value (first)

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if sentence == "":
-        sentence[0] = None
+        first = None
+    else:
+        first = sentence[0]
     length = len(sentence)
-    first = sentence[0]
     return (length, first)
+
