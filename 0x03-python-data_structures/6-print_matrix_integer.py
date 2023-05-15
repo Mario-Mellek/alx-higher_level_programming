2003eddcb0898011cacc24c1c093eaedc87e7e00
Mario-Mellek <mario.samy266@gmail.com>
2023-05-15 19:52:59 +0000

    handled spaces at the end of rows

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if matrix is None:
+        matrix = [[]]
     for i in matrix:
-        for j in i:
-            print("{:d}".format(j), end=" ")
+        for j in range(len(i)):
+            print("{:d}".format(i[j]), end="")
+            if j != len(i) - 1:
+                print(" ", end="")
         print()
