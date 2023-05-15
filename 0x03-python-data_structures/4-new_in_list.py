cbe1b9cb987ea7093ddf9fdfd8783229cea5a875
Mario-Mellek <mario.samy266@gmail.com>
2023-05-15 18:37:12 +0000

    checking return value

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,8 +2,6 @@
 def new_in_list(my_list, idx, element):
     my_listCopy = my_list.copy()
     if idx < 0 or idx >= len(my_list):
-        return my_list
-
+        return my_listCopy
     my_listCopy[idx] = element
     return my_listCopy
-
