f54a2b1dfff3d77f177fcdc2e147d94c4bb76692
Mario-Mellek <mario.samy266@gmail.com>
2023-05-18 00:42:16 +0000

    Handled edge case where more than one key has the max value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        maxKey = max(a_dictionary, key = a_dictionary.get)
-        return maxKey
+        maxVal = max(a_dictionary.values())
+        maxKeys = [key for key, value in a_dictionary.items()
+                   if value == maxVal]
+        if len(maxKeys) > 1:
+            return maxKeys
+        elif len(maxKeys) == 1:
+            return maxKeys[0]
     else:
         return None
