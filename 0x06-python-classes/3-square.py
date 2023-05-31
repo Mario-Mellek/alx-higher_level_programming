7c370162d0b2dfc9f1a95f3ab5026f56af69d4b1
Mario-Mellek <mario.samy266@gmail.com>
2023-05-31 00:21:49 +0000

    Computes the area of the square.

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""A class Square."""
+
+
+class Square:
+    """Represents a square with a default size."""
+
+    def __init__(self, size=0):
+        """Initializes a Sqaure.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        if type(size) is not (int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """
+        Computes the area of the square.
+
+        Returns:
+            The area of the square as an integer.
+        """
+        return self.__size ** 2
