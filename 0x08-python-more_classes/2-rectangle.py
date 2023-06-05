c72b07619114a47f161b180d3432b3a09a4b953b
Mario-Mellek <mario.samy266@gmail.com>
2023-06-05 20:02:34 +0000

    Added a conditional statment to check width or hight

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -86,6 +86,8 @@ class Rectangle:
         """
         Public instance method
 
-        Return: returns the rectangle area
+        Return: returns the rectangle perimeter
         """
+        if self.width == 0 or self.height == 0:
+            return 0
         return (2 * (self.width + self.height))
