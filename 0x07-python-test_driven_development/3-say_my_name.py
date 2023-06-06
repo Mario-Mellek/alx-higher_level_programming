d0f14ddcf96e6cb21922bfb7b054b5a6b8233f40
Mario-Mellek <mario.samy266@gmail.com>
2023-06-06 17:50:53 +0000

    Created and tested a function that prints My name is <first name> <last name>

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+The module contains a function that prints
+My name is <first name> <last name>
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    A unction that prints My name is <first name> <last name>
+
+    Args:
+        first_name (str): first_name arg
+        last_name (str): second_name arg
+
+    Raises:
+        TypeError: first and last names must be a string
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    if len(last_name) > 0:
+        print("My name is {} {}".format(first_name, last_name))
+    else:
+        print("My name is {}".format(first_name))
