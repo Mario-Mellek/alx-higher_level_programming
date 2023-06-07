62241edf77355bc22a57cdd8848d8a754b6d4b31
Mario-Mellek <mario.samy266@gmail.com>
2023-06-07 20:19:57 +0000

    editing unittest to include max at the begining and middle

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -8,12 +8,13 @@ class TestMaxInteger(unittest.TestCase):
     def test_randomVals(self):
         self.assertEqual(max_integer([2, 5, 10]), 10)
         self.assertEqual(max_integer([-100, -5, 10]), 10)
-        self.assertEqual(max_integer([-0, +0, 10]), 10)
+        self.assertEqual(max_integer([10, -0, +0]), 10)
         self.assertEqual(max_integer([10]), 10)
-        self.assertEqual(max_integer([10, 10.5, 10.9]), 10.9)
+        self.assertEqual(max_integer([10, 10.9, 10.5]), 10.9)
+        self.assertEqual(max_integer([]), None)
+
 
     def test_raisedErrors(self):
         self.assertRaises(TypeError, max_integer, [1, 2, "6"])
         self.assertRaises(TypeError, max_integer, [6, None])
         self.assertRaises(TypeError, max_integer, [6, 4, [2, 4]])
-
