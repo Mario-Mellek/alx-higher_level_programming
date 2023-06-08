724c9913d63db4f37271f002a208b25cfcf412f9
Mario-Mellek <mario.samy266@gmail.com>
2023-06-08 02:34:12 +0000

    added module docstring

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
+"""
+Module conatins LockedClass
+class doesn't allow users to arbitary create instances of it
+"""
+
+
 class LockedClass:
     """
     LockedClass: prevents the user from dynamically
     creating new instance attributes unless they're called:
     first_name
     """
-    __slots__ = ["first_name"]
+    __slots__ = ("first_name")
 
     def __init__(self):
         pass
