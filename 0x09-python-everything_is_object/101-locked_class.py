533201d7e3f17b3b78de428270404d4dcfa591f7
Mario-Mellek <mario.samy266@gmail.com>
2023-06-08 02:26:27 +0000

    added class docstrings

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 class LockedClass:
+    """
+    LockedClass: prevents the user from dynamically
+    creating new instance attributes unless they're called:
+    first_name
+    """
     __slots__ = {"first_name"}
 
     def __init__(self):
         pass
+
