37abfb4ddc8aad119407e20a85554d18c572f17f
Mario-Mellek <mario.samy266@gmail.com>
2023-06-13 01:20:15 +0000

    inherits __str__

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This module contains a Square class that inherits from Rectangle.
+"""
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """
+    A class that represents a square and inherits from Rectangle.
+    """
+    def __init__(self, size):
+        """
+        Initializes a new Square object.
+
+        Args:
+            size (int): The size of the square.
+        """
+        super().integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
