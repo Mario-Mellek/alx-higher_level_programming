4a106174c8f6a9c80fc97d5cc1914ffb2200c3cd
Mario-Mellek <mario.samy266@gmail.com>
2023-06-14 00:18:41 +0000

    A class Student that defines a student by: (based on 9-student.py)
    Public method def to_json(self, attrs=None): that retrieves
    a dictionary representation of a Student instance:
    If attrs is a list of strings,
    only attribute names contained in this list must be retrieved.
    Otherwise, all attributes must be retrieved

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+This module contains a definition of a class Student
+"""
+
+
+class Student:
+    """
+    Defines a student.
+
+    Public instance attributes:
+        first_name (str): The student's first name.
+        last_name (str): The student's last name.
+        age (int): The student's age.
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a Student object.
+
+        Args:
+            first_name (str): The student's first name.
+            last_name (str): The student's last name.
+            age (int): The student's age.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of a Student instance.
+
+        Returns:
+            dict: A dictionary representation of the Student instance.
+        """
+        if attrs:
+            result = {}
+            for attr in attrs:
+                if hasattr(self, attr):
+                    result[attr] = getattr(self, attr)
+            return result
+        else:
+            return self.__dict__
