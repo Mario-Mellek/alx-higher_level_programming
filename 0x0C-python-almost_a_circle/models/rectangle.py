00f8b676add5834a521d6ea416ac47bacbebc2bf
Mario-Mellek <mario.samy266@gmail.com>
2023-06-19 18:47:52 +0000

    edited init method

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -27,10 +27,10 @@ class Rectangle(Base):
             id (int): id of the rectangle object.
         """
         super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     @property
     def width(self):
