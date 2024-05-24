1083de5114f485bbc71642877ca81f8a6db9aebf
Mario-Mellek <mario.samy266@gmail.com>
2024-05-24 01:47:31 +0000

    edited column's name

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,3 +1,3 @@
 -- script that displays the average temperature by city ordered by temperature.
-SELECT city, AVG(temp) as avg_temp FROM temperatures GROUP BY city
+SELECT city, AVG(value) as avg_temp FROM temperatures GROUP BY city
 ORDER BY avg_temp DESC;
