56e94877018a01053686ba39a04bc31bc20c6e83
Mario-Mellek <mario.samy266@gmail.com>
2024-06-20 00:15:01 +0000

    created method charPrint(c) that prints the rectangle

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (!c) {
+      c = 'X';
+    }
+
+    for (let i = 0; i < this.width; i++) {
+      let square = '';
+      for (let j = 0; j < this.width; j++) {
+        square += c;
+      }
+      console.log(square);
+    }
+  }
+}
+module.exports = Square;
